7bbee94c783b7cb20bd6b0d1fe39407e4f174cca
Leonardo Furlan <leonardo.furlan@live.com>
2018-02-22 12:26:19 -0300

    Exemplos de operadores

---
+++ exemplo-01.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = "hcode";
+
+echo $nome . " mais alguma coisa<br>";
+
+$nome .= " Treinamentos";
+
+echo $nome;
+
+?>
---
+++ exemplo-02.php
@@ -0,0 +1,29 @@
+<?php
+
+$valorTotal = 0;
+
+$valorTotal += 100;
+
+$valorTotal += 25;
+
+//$valorTotal -= 10;
+
+$valorTotal *= .9;
+
+echo $valorTotal;
+
+$a = 1;
+
+$b = 2;
+
+var_dump($a < $b);
+
+//operadores
+
+// = é operador de atribuição
+
+// == compara apenas o valor
+
+// === compara valor e tipo
+
+?>
